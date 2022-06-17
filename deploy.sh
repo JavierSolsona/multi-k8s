100b8cbb2163aeac6d03afd26da1c46450475d07
JavierSolsona <javierdavid.394@gmail.com>
2022-06-17 00:40:20 -0400

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t javiersolsona/multi-client:latest -t javiersolsona/multi-client:$SHA -f ./client/Docerfile ./client
+docker build -t javiersolsona/multi-server:latest -t javiersolsona/multi-server:$SHA -f ./server/Docerfile ./server
+docker build -t javiersolsona/multi-worker:latest -t javiersolsona/multi-worker:$SHA -f ./worker/Docerfile ./worker
+
+docker push javiersolsona/multi-client:latest
+docker push javiersolsona/multi-server:latest
+docker push javiersolsona/multi-worker:latest
+
+docker push javiersolsona/multi-client:$SHA
+docker push javiersolsona/multi-server:$SHA
+docker push javiersolsona/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=javiersolsona/multi-server:$SHA
+kubectl set image deployments/client-deployment client=javiersolsona/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker= javiersolsona/multi-worker:$SHA
